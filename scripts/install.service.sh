d3f9651e3c8a1b438cd1445424b083acd861a004
alfu32 <ioan.alferaru@gmail.com>
2023-02-13 23:26:18 +0100

    feat(service): finally !

--- scripts/install.service.sh
+++ scripts/install.service.sh
@@ -16,33 +16,42 @@ ExecStart=$CWD/build/parentalcontrols.service.run
 WantedBy=multi-user.target
 SERVICEDEF
 
-
-cat > build/parentalcontrols.service.run << SERVICEDEF
+{
+cat << SERVICEDEF
 #!/bin/bash
 
 SUPER_HOME=$CWD/build
 CDIR=\`pwd\`
+
+####################### copying environment of $TARGET_USER
 SERVICEDEF
-for line in `env`;do
-    echo "export $line" >> build/parentalcontrols.service.run
+
+for procid in `pgrep -u $TARGET_USER gnome-session`;do
+    cat /proc/$procid/environ | egrep -z DBUS_SESSION_BUS_ADDRESS
+    echo ""
 done
-cat >> build/parentalcontrols.service.run << SERVICEDEF
-#### eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $TARGET_USER gnome-session)/environ)"
+
+echo ""
+cat << SERVICEDEF
+####################### done copying environment of $TARGET_USER
+#### eval \"export \\$\(egrep -z DBUS_SESSION_BUS_ADDRESS \\"/proc/\\$\(pgrep -u $TARGET_USER gnome-session)/environ\)\\"\)\"
 #### export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 cd \$SUPER_HOME # /home/$TARGET_USER/parental-controls/
 
 DATE=\`date +'%Y/%m/%d'\`
 LOGS="logs/\$DATE"
 
-./build/parental-controls-$arch.bin
+./parental-controls-$arch.bin
 
 cd \$CDIR
 SERVICEDEF
-
+} > build/parentalcontrols.service.run
 chmod +x build/parentalcontrols.service.run
 
+sudo systemctl disable parentalcontrols
 sudo systemctl stop parentalcontrols
 sudo cp build/parentalcontrols.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start parentalcontrols
-systemctl status parentalcontrols
+sudo systemctl enable parentalcontrols
+
