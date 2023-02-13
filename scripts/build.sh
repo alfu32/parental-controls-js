77820b959ada92e757e1c3d0786247dbbf27d1dd
alfu32 <ioan.alferaru@gmail.com>
2023-02-13 22:56:20 +0100

    feat(service)

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,36 +5,6 @@ CWD=`pwd`
 TARGET_USER=$1
 arch=$2
 
-
-cat > build/parentalcontrols.service << SERVICEDEF
-[Unit]
-Description=Parental Controls
-
-[Service]
-ExecStart=$CWD/build/parentalcontrols.service.run
-
-[Install]
-WantedBy=multi-user.target
-SERVICEDEF
-
-
-cat > build/parentalcontrols.service.run << SERVICEDEF
-#!/bin/bash
-
-SUPER_HOME=$CWD/build
-CDIR=\`pwd\`
-#### eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u devlin gnome-session)/environ)"
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-cd \$SUPER_HOME # /home/$TARGET_USER/parental-controls/
-
-DATE=\`date +'%Y/%m/%d'\`
-LOGS="logs/\$DATE"
-
-./build/parental-controls-$arch.bin
-
-cd \$CDIR
-SERVICEDEF
-
 ### for arch in x86_64-unknown-linux-gnu x86_64-pc-windows-msvc x86_64-apple-darwin aarch64-apple-darwin; do
 ### done
     echo "ARCHITECTURE [$arch]"
@@ -49,10 +19,10 @@ SERVICEDEF
     echo "deno build --allow-all --target \"$arch\" -o \"$OUT\" src/main.ts"
     deno compile --unstable --node-modules-dir --allow-all --target "$arch" -o "$OUT" src/main.ts
 
-deno bundle src/main.ts > build/parental-controls.app.deno.js
-cp scripts/parental-controls.app.deno.sh build/
+## deno bundle src/main.ts > build/parental-controls.app.deno.js
+## cp scripts/parental-controls.app.deno.sh build/
 cp config.json build/
-cp -r node_modules build/
+## cp -r node_modules build/
 ########## cp scripts/parental-controls.service.run build/
 ########## cp scripts/parentalcontrols.service build/
 ########## cp build/parental-controls.app.deno.js ./
--- scripts/install.service.sh
+++ scripts/install.service.sh
@@ -1,3 +1,46 @@
+#!/bin/bash
+
+CWD=`pwd`
+TARGET_USER=$1
+arch=$2
+
+
+cat > build/parentalcontrols.service << SERVICEDEF
+[Unit]
+Description=Parental Controls
+
+[Service]
+ExecStart=$CWD/build/parentalcontrols.service.run
+
+[Install]
+WantedBy=multi-user.target
+SERVICEDEF
+
+
+cat > build/parentalcontrols.service.run << SERVICEDEF
+#!/bin/bash
+
+SUPER_HOME=$CWD/build
+CDIR=\`pwd\`
+SERVICEDEF
+for line in `env`;do
+    echo "export $line" >> build/parentalcontrols.service.run
+done
+cat >> build/parentalcontrols.service.run << SERVICEDEF
+#### eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $TARGET_USER gnome-session)/environ)"
+#### export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+cd \$SUPER_HOME # /home/$TARGET_USER/parental-controls/
+
+DATE=\`date +'%Y/%m/%d'\`
+LOGS="logs/\$DATE"
+
+./build/parental-controls-$arch.bin
+
+cd \$CDIR
+SERVICEDEF
+
+chmod +x build/parentalcontrols.service.run
+
 sudo systemctl stop parentalcontrols
 sudo cp build/parentalcontrols.service /lib/systemd/system/
 sudo systemctl daemon-reload
