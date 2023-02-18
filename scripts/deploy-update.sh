960887318a998a71644b31dbda27390b198849e6
alfu32 <ioan.alferaru@gmail.com>
2023-02-18 23:10:35 +0100

    fix(scripts)

--- scripts/deploy-update.sh
+++ scripts/deploy-update.sh
@@ -1,24 +1,31 @@
 
-SSH_HOST=$2
+SSH_HOST=$1
 
-SSH_USER=$1
+SSH_USER=$2
+CWD=`pwd`
+ADMIN_USER=$2
+TARGET_USER=$3
+arch=$4
+DS=`date +'%Y%m%d%H%M%S'`
 
 #echo -n SSH Password :
 #read -s SSH_PWD
 
-SSH_CONNECTION="$SSH_USER:$SSH_PWD@$SSH_HOST"
 SSH_CONNECTION="$SSH_USER@$SSH_HOST"
 
 echo ""
 echo $SSH_CONNECTION
 echo ""
+SSH_TARGET=$SSH_CONNECTION:/home/$ADMIN_USER/parental-controls
 
+ssh $SSH_CONNECTION 'systemctl --user stop parentalcontrols'
+ssh $SSH_CONNECTION "mkdir '/home/$ADMIN_USER/parental-controls'"
+scp "./build/parental-controls-$arch.bin" $SSH_TARGET
+# scp ./config.json $SSH_TARGET
+scp "./build/parentalcontrols.service.run" $SSH_TARGET
+scp "./build/parentalcontrols.service" $SSH_TARGET
+scp "./build/parentalcontrols.service.install" $SSH_TARGET
+scp "./build/release.info.json "$SSH_TARGET
+ssh $SSH_CONNECTION 'systemctl --user start parentalcontrols'
+# ssh -S $SSH_CONNECTION 'systemctl --user start parentalcontrols'
 ssh $SSH_CONNECTION 'systemctl status parentalcontrols'
-ssh -S $SSH_CONNECTION 'sudo systemctl stop parentalcontrols'
-ssh -S $SSH_CONNECTION 'sudo systemctl disable parentalcontrols'
-ssh $SSH_CONNECTION 'mkdir parental-controls'
-scp ./build/parental-controls-x86_64-unknown-linux-gnu.bin $SSH_CONNECTION:/home/$SSH_USER/parental-controls
-scp ./config.json $SSH_CONNECTION:/home/$SSH_USER/parental-controls
-scp ./build/parentalcontrols.service.run $SSH_CONNECTION:/home/$SSH_USER/parental-controls
-scp ./build/parentalcontrols.service $SSH_CONNECTION:/home/$SSH_USER/parental-controls
-scp ./build/parentalcontrols.service.install $SSH_CONNECTION:/home/$SSH_USER/parental-controls
