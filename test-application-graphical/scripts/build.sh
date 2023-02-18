a4ecd8a7a43113aaf89372157e13aaccc45dd953
alfu32 <ioan.alferaru@gmail.com>
2023-02-18 08:59:26 +0100

    feat(build-scripts)

---
+++ test-application-graphical/scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DS=`date +i'%Y%m%d%H%M%S'`
+scripts/./clean.sh
+cd ./bin
+cmake .. "-DCMAKE_TOOLCHAIN_FILE=/home/alfu64/.vcpkg/scripts/buildsystems/vcpkg.cmake"
+make
+{
+    echo "BUILD_DATE=$DS"
+    echo "COMMIT_ID=$(git rev-parse HEAD)"
+    echo "BRANCH=$(git rev-parse --abbrev-ref HEAD)"
+    echo "TAG=$(git rev-parse --abbrev-ref HEAD)"
+} > build
+cd ..
+
+scripts/./clean.sh
