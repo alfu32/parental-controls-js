aff707be42e6cabfae175930d6270ddfe3fbd4f7
alfu32 <ioan.alferaru@gmail.com>
2023-02-18 11:05:26 +0100

    fix(scripts)

--- parental-controls-ui/scripts/build.sh
+++ parental-controls-ui/scripts/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-DS=`date +i'%Y%m%d%H%M%S'`
-rm -rf build
+DS=`date +'%Y%m%d%H%M%S'`
+rm -rf dist
 vite build
+cp ../scripts/fileserve-static.sh dist/
 . "../scripts/semver.sh"
 {
-    echo "BUILD_DATE=$DS"
-    echo "COMMIT_ID=$(git rev-parse HEAD)"
-    echo "BRANCH=$(git rev-parse --abbrev-ref HEAD)"
-    echo "TAG=$(git describe --tags)"
-    echo "SEMVER=$SEMVER"
-} > build/build
+    echo "{"
+    echo "    \"BUILD_DATE\":\"$DS\","
+    echo "    \"COMMIT_ID\":\"$(git rev-parse HEAD)\","
+    echo "    \"BRANCH\":\"$(git rev-parse --abbrev-ref HEAD)\","
+    echo "    \"TAG\":\"$(git describe --tags)\","
+    echo "    \"SEMVER\":\"$SEMVER\""
+    echo "}"
+} > dist/build.json
--- scripts/fileserve-static.sh
+++ scripts/fileserve-static.sh
@@ -1,34 +1,125 @@
 #!/bin/bash
 
-PORT=$2
-ROOT_DIR=$1
+## Create the response FIFO
+rm -f response
+mkfifo response
 
+function handle_GET_home() {
+  RESPONSE=$(cat home.html | \
+    sed "s/{{$COOKIE_NAME}}/$COOKIE_VALUE/")
+}
 
+function handle_GET_login() {
+  RESPONSE=$(cat login.html)
+}
 
-while true; do
-  # Wait for a client to connect
-  client_socket=$(nc -l $PORT)
+function handle_POST_login() {
+  RESPONSE=$(cat post-login.http | \
+    sed "s/{{cookie_name}}/$INPUT_NAME/" | \
+    sed "s/{{cookie_value}}/$INPUT_VALUE/")
+}
 
-  # Parse the HTTP request
-  read -r request_line <&$client_socket
-  method=$(echo $request_line | cut -d ' ' -f 1)
-  path=$(echo $request_line | cut -d ' ' -f 2)
+function handle_POST_logout() {
+  RESPONSE=$(cat post-logout.http | \
+    sed "s/{{cookie_name}}/$COOKIE_NAME/" | \
+    sed "s/{{cookie_value}}/$COOKIE_VALUE/")
+}
 
-  # Serve the requested file
-  if [ "$method" = "GET" ]; then
-    file_path="$ROOT_DIR$path"
-    if [ -f "$file_path" ]; then
-      echo "HTTP/1.1 200 OK" >&$client_socket
-      echo "Content-Type: $(file -b --mime-type $file_path)" >&$client_socket
-      echo >&$client_socket
-      cat $file_path >&$client_socket
-    else
-      echo "HTTP/1.1 404 Not Found" >&$client_socket
-      echo >&$client_socket
-      echo "404 Not Found" >&$client_socket
-    fi
+function handle_not_found() {
+  A=$0
+  req=$1
+  path=$2
+  fpath="$ROOT_FOLDER$path"
+  if [ -f "$fpath" ];then
+  RESPONSE="HTTP/1.1 200
+Requested-Path: $path [$fpath]
+Content-Length: $(wc -c < $fpath)
+
+$(cat $fpath)
+"
+  else
+  RESPONSE="HTTP/1.1 404
+Content-Type: text/plain
+Content-Length: 10
+Requested-Path: $path
+
+$path not found
+"
+fi
+}
+
+function handleRequest() {
+  ## Read the HTTP request until \r\n
+  local LINENUM=1
+  while read line; do
+    trline=$(echo $line | tr -d '[\r\n]') ## Removes the \r\n from the EOL
+
+    ## Breaks the loop when line is empty
+    [ -z "$trline" ] && break
+
+    ## Parses the headline
+    ## e.g GET /login HTTP/1.1 -> GET /login
+    ##if [ "$LINE" == "1" ];then
+      echo $line
+      HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
+      [[ "$trline" =~ $HEADLINE_REGEX ]] &&
+        REQUEST=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1 \2/")
+      PATH_REGEX='^(.*?)\s(.*?)$'
+      [[ "$REQUEST" =~ $PATH_REGEX ]] &&
+        REQ_PATH=$(echo $REQUEST | sed -E "s/$PATH_REGEX/\2/")
+    ##fi
+    ## Parses the Content-Length header
+    ## e.g Content-Length: 42 -> 42
+    CONTENT_LENGTH_REGEX='Content-Length:\s(.*?)'
+    [[ "$trline" =~ $CONTENT_LENGTH_REGEX ]] &&
+      CONTENT_LENGTH=$(echo $trline | sed -E "s/$CONTENT_LENGTH_REGEX/\1/")
+
+    ## Parses the Cookie header
+    ## e.g Cookie: name=John -> name John
+    COOKIE_REGEX='Cookie:\s(.*?)\=(.*?).*?'
+    [[ "$trline" =~ $COOKIE_REGEX ]] &&
+      read COOKIE_NAME COOKIE_VALUE <<< $(echo $trline | sed -E "s/$COOKIE_REGEX/\1 \2/")
+    LINENUM=$(($LINENUM + 1))
+  done
+
+  ## Read the remaining HTTP request body
+  if [ ! -z "$CONTENT_LENGTH" ]; then
+    BODY_REGEX='(.*?)=(.*?)'
+
+    while read -n$CONTENT_LENGTH -t1 line; do
+      echo $line
+      trline=`echo $line | tr -d '[\r\n]'`
+
+      [ -z "$trline" ] && break
+
+      read INPUT_NAME INPUT_VALUE <<< $(echo $trline | sed -E "s/$BODY_REGEX/\1 \2/")
+    done
   fi
 
-  # Close the client socket
-  exec >&-
+  ## Route request to the response handler
+  case "$REQUEST" in
+    "GET /login")   handle_GET_login ;;
+    "GET /")        handle_GET_home ;;
+    "POST /login")  handle_POST_login ;;
+    "POST /logout") handle_POST_logout ;;
+    *)              handle_not_found "$REQUEST" "$REQ_PATH";;
+  esac
+
+  echo -e "$RESPONSE" > response
+}
+
+ROOT_FOLDER=$1
+PORT=$2
+
+echo "Listening on $PORT ..."
+
+## Keep the server running forever
+while true; do
+  ## 1. wait for FIFO
+  ## 2. creates a socket and listens to the port 3000
+  ## 3. as soon as a request message arrives to the socket, pipes it to the handleRequest function
+  ## 4. the handleRequest function processes the request message and routes it to the response handler, which writes to the FIFO
+  ## 5. as soon as the FIFO receives a message, it's sent to the socket
+  ## 6. closes the connection (`-N`), closes the socket and repeat the loop
+  cat response | nc -lN $PORT | handleRequest
 done
